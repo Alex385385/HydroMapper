5ab34e2d38a46e6076dcb334d24439c5b0efa3a0
Alejandro Millan <alejandro_millan@ymail.com>
2020-04-01 00:47:51 -0700

    Switched from Google Maps to Leaflet Maps

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,62 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:user_location/user_location.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Completer<GoogleMapController> _controller = Completer();
-
-  static const LatLng _center = const LatLng(34.057459, -117.822361);
-
-  void _onMapCreated(GoogleMapController controller) {
-    _controller.complete(controller);
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hydro Mapper'),
-          backgroundColor: Colors.blue[700],
-        ),
-        body: GoogleMap(
-          myLocationEnabled: true,
-          mapType: MapType.normal,
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 15.0,
-          ),
-        ),
+      title: 'User Location Plugin Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: HomePage(),
     );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  // ADD THIS
+  MapController mapController = MapController();
+  UserLocationOptions userLocationOptions;
+  // ADD THIS
+  List<Marker> markers = [];
+  @override
+  Widget build(BuildContext context) {
+    // You can use the userLocationOptions object to change the properties
+    // of UserLocationOptions in runtime
+    userLocationOptions = UserLocationOptions(
+      context: context,
+      mapController: mapController,
+      markers: markers,
+      updateMapLocationOnPositionChange: false,
+    );
+    return Scaffold(
+        appBar: AppBar(title: Text("Hydro Mapper")),
+        body: FlutterMap(
+          options: MapOptions(
+            center: LatLng(34.057459, -117.822361),
+            zoom: 20.0,
+            plugins: [
+              // ADD THIS
+              UserLocationPlugin(),
+            ],
+          ),
+          layers: [
+            TileLayerOptions(
+              urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+              subdomains: ['a', 'b', 'c'],
+            ),
+            // ADD THIS
+            MarkerLayerOptions(markers: markers),
+            // ADD THIS
+            userLocationOptions,
+          ],
+          // ADD THIS
+          mapController: mapController,
+        ));
   }
 }
